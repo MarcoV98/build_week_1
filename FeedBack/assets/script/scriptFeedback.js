b430b678b77e4dbdba9c7eaf4e8de4f2c8d3d0a9
Salvatore Ragosta <sasyragosta24@gmail.com>
2023-11-14 14:50:25 +0100

    FINE PAGINE FEEDBACK
    PAGINA COMPLETA DI HTML/CSS/JS

--- FeedBack/assets/script/scriptFeedback.js
+++ FeedBack/assets/script/scriptFeedback.js
@@ -1,17 +1,31 @@
-stelle = document.querySelectorAll("#contenitoreStelle svg")
-
 let svgs = document.querySelectorAll("#contenitoreStelle svg");
+let clicked = false;
 
 for (let i = 0; i < svgs.length; i++) {
   svgs[i].addEventListener('mouseover', function() {
-    for (let j = 0; j <= i; j++) {
-      svgs[j].style.fill = 'red';
+    if (!clicked) {
+      for (let j = 0; j <= i; j++) {
+        svgs[j].style.fill = '#00ffff';
+      }
+      }
+  });
+  svgs[i].addEventListener('mouseout', function() {
+    if (!clicked) {
+      for (let j = 0; j <= i; j++) {
+        svgs[j].style.fill = 'initial';
+      }
     }
   });
-
-  svgs[i].addEventListener('mouseout', function() {
+  svgs[i].addEventListener('click', function() {
+    if(clicked){
+      clicked=false
+      svgs.forEach(element => {
+        element.style.fill ="#000000"
+      });
+  }
+    clicked = true;
     for (let j = 0; j <= i; j++) {
-      svgs[j].style.fill = 'initial';
+      svgs[j].style.fill = '#00ffff';
     }
   });
 }
