c28e5bd0e97ac1899316d050710bc63f863fd1a6
MarcoV98 <148749261+MarcoV98@users.noreply.github.com>
2023-11-17 12:10:31 +0100

    "aggiunto feedback visibile"

--- FeedBack/assets/script/scriptFeedback.js
+++ FeedBack/assets/script/scriptFeedback.js
@@ -29,3 +29,30 @@ for (let i = 0; i < svgs.length; i++) {
     }
   });
 }
+
+function mostraCommento() {
+  let commento = document.getElementById("commento").value.trim()
+  let commentoOutput = document.getElementById("commentoOutput")
+
+  if (commento === "") {
+    let avviso = document.createElement("p")
+    avviso.id = "feedbackMessage"
+    avviso.textContent = "Please enter a comment before submitting."
+    avviso.style.color = "red"
+    commentoOutput.appendChild(avviso)
+    return;
+  }
+
+  let thankyouFeedback = document.createElement("div")
+  thankyouFeedback.id = "thankyouFeedack"
+
+  let commentoElement = document.createElement("p")
+  commentoElement.innerHTML = '<p id="rateUs">Your comment:</p><p style="font-style: italic;">' + commento + '</p>'
+
+  thankyouFeedback.innerHTML = '<h3>Thank you for your feedback!</h3>'
+  thankyouFeedback.appendChild(commentoElement)
+
+  commentoOutput.innerHTML = " "
+  commentoOutput.appendChild(thankyouFeedback)
+  document.getElementById("commento").value = ""
+}
